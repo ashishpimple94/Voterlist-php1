e88320758c8448985dbc2fdec38d4ebd9482ba43
ashishpimple <ashish@hola9.com>
2025-11-01 11:11:05 +0530

    Remove all PHP files - project now pure Node.js

--- public/health.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-echo json_encode([
-    'status' => 'ok',
-    'timestamp' => date('Y-m-d H:i:s'),
-    'service' => 'Voter API',
-    'version' => '1.0'
-], JSON_PRETTY_PRINT);
--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Ultra simple - no dependencies
-header('Content-Type: application/json');
-echo json_encode(['status' => 'ok', 'message' => 'Working!']);
-exit;
